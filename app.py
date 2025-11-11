1af6aae299850fc7c4bed9e57fcf3d2c6b029f0d
aysenurunal <131713344+aysenurunal@users.noreply.github.com>
2025-11-11 17:43:23 +0300

    update db url

--- app.py
+++ app.py
@@ -366,5 +366,6 @@ def make_proposal(offer_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000,debug=True)
-
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000, debug=True)
